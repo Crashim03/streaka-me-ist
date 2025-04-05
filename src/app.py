23225b448762d60952aefbb16733a5e093ffeec7
Ana Margarida Almeida <ana.margarida2004@gmail.com>
2025-04-05 03:14:35 +0100

    update port and some wip

--- src/app.py
+++ src/app.py
@@ -1,62 +1,80 @@
 import os
 from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 mongo_uri = "mongodb://root:example@mongo:27017/"
 client = MongoClient(mongo_uri)
 db = client.bebidas
 colecao = db.contador
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 dados = {}
-registros = []
 
-# Inicializa valores se não existirem
-def inicializar_dados():
-    print("Inicializando dados...")
-    if colecao.count_documents({}) == 0:
-        colecao.insert_many([
-            {"nome": "tu", "quantidade": 0},
-            {"nome": "amigo", "quantidade": 0}
-        ])
 
 @app.route("/")
 def index():
     print("Acessando a página inicial...")
-    inicializar_dados()
-    dados = {item["nome"]: item["quantidade"] for item in colecao.find()}
+    dados = colecao.find()
     return render_template("index.html", dados=dados)
 
 
-@app.route('/upload-imagem', methods=['POST'])
-def upload_imagem():
-    if 'imagem' not in request.files:
-        return jsonify({'erro': 'Nenhum arquivo encontrado'}), 400
-    file = request.files['imagem']
-    if file.filename == '':
-        return jsonify({'erro': 'Arquivo sem nome'}), 400
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+@app.route("/upload-imagem-ana", methods=["POST"])
+def upload_imagem_ana():
+    return upload_imagem("Ana")
+
+
+@app.route("/upload-imagem-alex", methods=["POST"])
+def upload_imagem_alex():
+    return upload_imagem("Alex")
+
+
+def upload_imagem(nome_usuario):
+    if "imagem" not in request.files:
+        return jsonify({"erro": "Nenhum arquivo encontrado"}), 400
+    file = request.files["imagem"]
+    if file.filename == "":
+        return jsonify({"erro": "Arquivo sem nome"}), 400
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
     file.save(filepath)
-    return jsonify({'mensagem': 'Upload feito com sucesso', 'caminho': filepath})
+
+    # Adicionando o registro de upload na base de dados
+    new_entry = {
+        "nome": nome_usuario,
+        "data": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "acao": "Adicionado",
+        "imagem_path": filepath,
+    }
+    colecao.insert_one(new_entry)
+
+    return jsonify({"mensagem": "Upload feito com sucesso", "caminho": filepath})
+
 
 @app.route("/atualizar", methods=["POST"])
 def atualizar():
-    data = request.json.get()
-    nome = data['nome']
-    delta = data['delta']
-    
-    # Atualiza o valor de 'tu' ou 'amigo' (armazena no dicionário)
-    dados[nome] += delta
-    
-    # Adiciona o registro de ação
-    acao = 'Adicionado' if delta > 0 else 'Removido'
-    registros.append({
-        'data': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'quem': nome.capitalize(),
-        'acao': f"{acao} {abs(delta)}"
-    })
-    
+    print("Atualizando dados...")
+    nome = request.json.get("nome")
+    delta = request.json.get("delta")
+
+    print("nome:", nome)
+
+    new_entry = {
+        "nome": nome,
+        "data": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "acao": "Adicionado" if delta > 0 else "Removido",
+    }
+
+    colecao.insert_one(new_entry)
+
+    # retorna a quantidade do nome dado
+    quantidade = colecao.count_documents({"nome": nome, "acao": "Adicionado"})
+    quantidade -= colecao.count_documents({"nome": nome, "acao": "Removido"})
+
     # Retorna o novo valor para o frontend
-    return jsonify({'quantidade': dados[nome]})
+    return jsonify({"quantidade": quantidade})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
